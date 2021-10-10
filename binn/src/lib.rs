8751b5b339035d7454fde7366ccb6b04999d6c7d
Alfonso Ros <alfonso.ros@esrlabs.com>
2021-10-10 20:59:09 +0200

    p2.1: Create a binn object

--- binn/src/lib.rs
+++ binn/src/lib.rs
@@ -1,7 +1,29 @@
+pub struct BinnObject(*mut binn_sys::binn);
+
+impl BinnObject {
+    pub fn new() -> Self {
+        Self(unsafe { binn_sys::binn_object() })
+    }
+}
+
+impl Drop for BinnObject {
+    fn drop(&mut self) {
+        unsafe { binn_sys::binn_free(self.0) };
+    }
+}
+
+impl Default for BinnObject {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn dummy_test() {
-        assert_eq!(2 + 2, 4);
+    fn create_object_test() {
+        let _binn = BinnObject::new();
     }
 }
