35f7e57ba1cdc7d497d1985a4bb3d72f3ed37ae4
Alfonso Ros <alfonso.ros@esrlabs.com>
2021-10-19 19:59:57 +0200

    p1.0: Use bindgen to create bindings

---
+++ binn-sys/build.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .generate()
+        .expect("failed to generate bindings");
+
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("failed to write bindings");
+}
